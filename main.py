1f6921a2186d23074ad306fe2c07e6a58a3b3ab1
elp49 <elp49@drexel.edu>
2020-11-23 13:28:29 -0500

    updating agent action choice to train Q-table and find optimal actions

--- main.py
+++ main.py
@@ -1,19 +1,54 @@
 import importlib
 import sys
 
+# *
+## === ADDING IMPORTS === ##
+# *
+
+import time
+
+# *
+## === === ====== === === ##
+# *
+
 from util import Arguments
 
 if __name__ == '__main__':
 
     args = Arguments()
 
+#//# ===== ===== ==== = === = === = ==== ===== ===== #\\#
+#//# ===== = = == === ORIGINAL CODE === == = = ===== #\\#
+#//# ===== ===== ==== = === = === = ==== ===== ===== #\\#
+    # player = args.get('player', 'agent')
+    # screen = args.get('screen', 'medium')
+    # steps = args.get_int('steps', None)
+    # train = args.get('train', None)
+    # speed = args.get('speed', 'fast' if train else 'slow')
+    # restart = args.get_int('restart', None)
+    # output = args.get('output', 'graphics')
+#//# ===== ===== ==== = === = === = ==== ===== ===== #\\#
+#//# ===== = = == === ************* === == = = ===== #\\#
+#//# ===== ===== ==== = === = === = ==== ===== ===== #\\#
+
+
+
+    ## ===== ===== = ===== ===== ##
+    SCREEN = 'medium' # finished after 19.04 minutes with 
+    # a score of 969605
+    STEPS = 100000
+    TRAINING_FILE = 'q8'
+    RESTART = 4
+    OUTPUT = 'text'
+
     player = args.get('player', 'agent')
-    screen = args.get('screen', 'medium')
-    steps = args.get_int('steps', None)
-    train = args.get('train', None)
-    speed = args.get('speed', 'fast' if train else 'slow')
-    restart = args.get_int('restart', None)
-    output = args.get('output', 'graphics')
+    screen = args.get('screen', SCREEN)
+    steps = args.get_int('steps', STEPS)
+    train = args.get('train', TRAINING_FILE)
+    speed = args.get('speed', 'fast')
+    restart = args.get_int('restart', RESTART)
+    output = args.get('output', OUTPUT)
+    ## ===== ===== = ===== ===== ##
 
     from frogger.settings import settings
     settings['use_graphics'] = (output.lower() != 'text')
@@ -26,5 +61,29 @@ if __name__ == '__main__':
         agent = agent_module.Agent(train=train)
         game.add_agent(agent)
 
+
+    ## ===== ===== = ===== ===== ##
+    ## === == =========== == === ##
+    ## ===== ===== = ===== ===== ##
+    then = time.time()
+    ## ===== ===== = ===== ===== ##
+    ## === == =========== == === ##
+    ## ===== ===== = ===== ===== ##
+
+
     scores = game.run(steps=steps, speed=speed, restart=restart)
+    
+
+    ## ===== ===== = ===== ===== ##
+    ## === == =========== == === ##
+    ## ===== ===== = ===== ===== ##
+    now = time.time()
+    seconds = now - then
+    minutes = seconds / 60
+    print(f'{round(minutes, 2)} minutes')
+    ## ===== ===== = ===== ===== ##
+    ## === == =========== == === ##
+    ## ===== ===== = ===== ===== ##
+
+
     print('\t'.join([str(score) for score in scores]))
